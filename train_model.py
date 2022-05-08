d550b6b40815c99c1373300daa6964ed035b810e
jonathanlevine <jonathanaaron.levine@gmail.com>
2022-05-08 01:49:21 -0400

    Updated training and testing sets. Added training on GPU

--- train_model.py
+++ train_model.py
@@ -4,13 +4,19 @@ import numpy as np
 import pandas
 import time
 from torch.utils.data import Dataset, DataLoader
+import bz2
+import pickle
+import _pickle as cPickle
+
+# Better accuracy is needed!
+torch.backends.cuda.matmul.allow_tf32 = False
 
 
+# Use the GPU for training
 if torch.cuda.is_available():
     dev = "cuda:0"
 else:
     dev = "cpu"
-
 
 device = torch.device(dev)
 
@@ -24,15 +30,20 @@ def get_labels(dataframe:pandas.DataFrame)->torch.Tensor:
 
 
 def transform_labels(values):
-    return np.log10(np.abs(values))
+    return 1/np.log10(np.abs(values))
 
 
-def percent_error(array1, array2)->float:
-    return 0
+# Pickle a file and then compress it into a file with extension 
+def compressed_pickle(title, data):
+    with bz2.BZ2File(title + '.pbz2', 'w') as f: 
+        cPickle.dump(data, f)
 
 
-def loss_function(labels, target)->float:
-    return 0
+# Load any compressed pickle file
+def decompress_pickle(file):
+    data = bz2.BZ2File(file, 'rb')
+    data = cPickle.load(data)
+    return data
 
 
 class GratingCouplerDataset(torch.utils.data.Dataset):
@@ -69,7 +80,6 @@ class Network(nn.Module):
 
         def forward(self, x:[])->[]:
                 x = self.input(x)
-                x = self.relu(x)
                 x = self.first_hidden(x)
                 x = self.relu(x)
                 x = self.second_hidden(x)
@@ -85,15 +95,6 @@ class Network(nn.Module):
                 return x
 
 
-class RMSLELoss(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.mse = nn.MSELoss()
-
-    def forward(self, pred, actual):
-        return torch.sqrt(self.mse(torch.log(pred+1), torch.log(actual+1)))
-
-
 class MSELoss(nn.Module):
     def __init__(self):
         super().__init__()
@@ -103,48 +104,33 @@ class MSELoss(nn.Module):
         return self.mse(pred, actual)
 
 
-class mean_absolute_error(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, pred, actual):
-        return torch.mean(torch.abs((pred - actual) / actual))
-
-
 # Get the start time
 start_time = time.time()
 
 # Load the dataset from saved CSV
-training_set = pandas.read_csv('DATASETS/NOISE/TRAINING_24March2022.csv')
+training_set = decompress_pickle('datasets/training_set_normalized_features.pbz2')
 training_set = training_set.sample(frac=1)
-testing_set = pandas.read_csv('DATASETS/NOISE/TESTING_24March2022.csv')
+
+testing_set = decompress_pickle('datasets/testing_set_normalized_features.pbz2')
 testing_set = testing_set.sample(frac=1)
 
 # TRAINING SET
 # Get the x, y values
-x = get_features(training_set)
-y = transform_labels(get_labels(training_set))
-
-x_training_set = torch.tensor(x, dtype=torch.float32)
-y_training_set = torch.tensor(y, dtype=torch.float32)
+x_train = torch.tensor(get_features(training_set), dtype=torch.float32).to(device)
+y_train = torch.tensor(transform_labels(get_labels(training_set)), dtype=torch.float32).to(device)
 
 # TESTING SET
 # Get the x, y values
-x_test = torch.tensor(get_features(testing_set), dtype=torch.float32)
-y_test = torch.tensor(transform_labels(get_labels(testing_set)), dtype=torch.float32)
-
-# Dataloader
-Dataset = GratingCouplerDataset(x, y)
-dataloader = DataLoader(dataset = Dataset, batch_size=10000)
+x_test = torch.tensor(get_features(testing_set), dtype=torch.float32).to(device)
+y_test = torch.tensor(transform_labels(get_labels(testing_set)), dtype=torch.float32).to(device)
 
 # MODEL AND PARAMETERS
-GratingCouplerNet = Network()
+GratingCouplerNet = Network().to(device)
 
 learning_rate = 0.001
+weight_decay = 0
 optimizer = torch.optim.Adam(GratingCouplerNet.parameters(), lr=learning_rate)
 
-
-mae_loss = mean_absolute_error()
 mse_loss = MSELoss()
 
 # Learning rate scehduler
@@ -153,13 +139,10 @@ learning_rate_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optim
 
 # INITLIAZE EPOCH AND LOSSES
 epoch = 0
-max_epoch = 250
+max_epoch = 500
 
 # Training loss
 loss = 1000
-
-# Valdiation loss
-validation_loss = 1000
 
 training_losses_mse = []
 testing_losses_mse = []
@@ -168,34 +151,22 @@ testing_losses_mae = []
 learning_rates = []
 
 # Train GratingCouplerNet
-for epoch in range(max_epoch):    
-    for i, (x_train, y_train) in enumerate(dataloader):
-        test_prediction = GratingCouplerNet(x_test)
+for epoch in range(max_epoch):
+    test_prediction = GratingCouplerNet(x_test)
 
-        # Evaluate the training MSE loss from the Training set
-        prediction = GratingCouplerNet(x_train)
-        
-        training_mse_error = mse_loss(prediction, y_train)
+    # EVALUATE THE TRAINING LOSS
+    prediction = GratingCouplerNet(x_train)
+    training_mse_error = mse_loss(prediction, y_train)
 
-        # EVALUATE THE TESTING LOSS FROM THE TESTING SET
-        testing_mse_error = mse_loss(test_prediction, y_test)
+    # EVALUATE THE TESTING LOSS
+    testing_mse_error = mse_loss(test_prediction, y_test)
 
-        # Zero the gradients in the Network
-        optimizer.zero_grad()
+    # ZERO THE GRADIENTS IN THE NETWORK
+    optimizer.zero_grad()
 
-        # Update the weights and step the optimizer
-        training_mse_error.backward()
-        optimizer.step()
-
-        print("Batch: {},"\
-              " Training Loss (MSE): {:0.6f},"\
-              " Testing Loss (MSE): {:0.6f}".format(i, training_mse_error, testing_mse_error))
-
-    # GET THE MSE
-    value1 = mse_loss(GratingCouplerNet(x_training_set), y_training_set).detach().numpy()
-    training_losses_mse.append(value1)
-    value2 = mse_loss(GratingCouplerNet(x_test), y_test).detach().numpy()
-    testing_losses_mse.append(value2)
+    # UPDATE THE WEIGHTS AND STEP THE OPTIMIZER
+    training_mse_error.backward()
+    optimizer.step()
 
     print("\nEpoch/Time: {}/{:0.6f}, "\
       "lr: {:0.8f}, "\
@@ -205,19 +176,12 @@ for epoch in range(max_epoch):
                                       (time.time()-start_time)/60,
                                        learning_rate_scheduler.get_last_lr()[0],
                                        weight_decay, 
-                                       value1, 
-                                       value2))
-
-    learning_rates.append(learning_rate_scheduler.get_last_lr()[0])
+                                       training_mse_error, 
+                                       testing_mse_error))
 
     # Save the model
-    # torch.save(GratingCouplerNet, 'DATASETS/test_network')
+    torch.save(GratingCouplerNet, 'models/GratingCouplerNetModel')
 
-    # # Save the losses to a dataframe and csv file
-    # d = {'training_loss_mse': training_losses_mse, 
-    #      'testing_loss_mse': testing_losses_mse, 
-    #      'learning_rate': learning_rates}
-    # dataframe = pandas.DataFrame(data=d)
-    # dataframe.to_csv('DATASETS/csvs/10k.csv')
+    torch.cuda.empty_cache()
 
 print("Execution time: {}".format(time.time() - start_time))
