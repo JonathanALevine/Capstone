53c93549bc783abfdbefa91048944c911b531369
jonathanlevine <jonathanaaron.levine@gmail.com>
2022-05-09 21:13:37 -0400

    batched SGD

--- train_model.py
+++ train_model.py
@@ -150,32 +150,32 @@ loss = 1000
 
 # Train GratingCouplerNet
 for epoch in range(max_epoch):
-    # for i, (x_train, y_train) in enumerate(dataloader):
-    test_prediction = GratingCouplerNet(x_test)
+    for i, (x_train, y_train) in enumerate(dataloader):
+        test_prediction = GratingCouplerNet(x_test)
 
-    # EVALUATE THE TRAINING LOSS
-    prediction = GratingCouplerNet(x)
-    training_mse_error = mse_loss(prediction, y)
+        # EVALUATE THE TRAINING LOSS
+        prediction = GratingCouplerNet(x)
+        training_mse_error = mse_loss(prediction, y)
 
-    # EVALUATE THE TESTING LOSS
-    testing_mse_error = mse_loss(test_prediction, y_test)
+        # EVALUATE THE TESTING LOSS
+        testing_mse_error = mse_loss(test_prediction, y_test)
 
-    # ZERO THE GRADIENTS IN THE NETWORK
-    optimizer.zero_grad()
+        # ZERO THE GRADIENTS IN THE NETWORK
+        optimizer.zero_grad()
 
-    # UPDATE THE WEIGHTS AND STEP THE OPTIMIZER
-    training_mse_error.backward()
-    optimizer.step()
+        # UPDATE THE WEIGHTS AND STEP THE OPTIMIZER
+        training_mse_error.backward()
+        optimizer.step()
 
-        # print("Batch: {},"\
-        #       " Training Loss (MSE): {:0.6f},"\
-        #       " Testing Loss (MSE): {:0.6f}".format(i, training_mse_error, testing_mse_error))
+        print("Batch: {},"\
+              " Training Loss (MSE): {:0.6f},"\
+              " Testing Loss (MSE): {:0.6f}".format(i, training_mse_error, testing_mse_error))
 
     print("\nEpoch/Time: {}/{:0.6f}, "\
-      "lr: {:0.8f}, "\
-      "wd: {:0.8f}, "\
-      "Training Loss: {:0.6f}, "\
-      "Testing Loss: {:0.6f}\n".format(epoch, 
+          "lr: {:0.8f}, "\
+          "wd: {:0.8f}, "\
+          "Training Loss: {:0.6f}, "\
+          "Testing Loss: {:0.6f}\n".format(epoch, 
                                       (time.time()-start_time)/60,
                                        learning_rate_scheduler.get_last_lr()[0],
                                        weight_decay, 
